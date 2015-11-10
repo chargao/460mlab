355d62f108d9d381dc8320be248c22160d8eaff7
chargao <charlie.gao@utexas.edu>
2015-11-10 17:42:00 -0600

    some early case stuff

--- lab6/7_seg.v
+++ lab6/7_seg.v
@@ -4,41 +4,40 @@ input clk_100Hz;
 output reg [3:0] an;
 output reg [6:0] segs;
 
-reg Digit_Select;
-reg [3:0] digit_to_Display;
+reg digit_select;
+reg [3:0] digit_to_display;
 
-initial begin Digit_Select <= 0; end
+initial begin digit_select <= 0; end
 
 always @(posedge clk_100Hz) begin
-  if(Digit_Select == 0) begin Digit_Select <= 1; end   // "10" digit
-  else 			begin Digit_Select <= 0; end   // "01" digit
+  digit_select <= ~digit_select; //"01" or "10" digit
 
-  case (Digit_Select) // Choose which Anode and cathode set to activate. ACTIVE LOW!! 
-  0: begin an <= 4'b1110; digit_to_Display <= bin_in[3:0];  end
-  1: begin an <= 4'b1101; digit_to_Display <= bin_in[7:4];  end
+  case (digit_select) // Choose which Anode and cathode set to activate. ACTIVE LOW!! 
+  0: begin an <= 4'b1110; digit_to_display <= bin_in[3:0];  end
+  1: begin an <= 4'b1101; digit_to_display <= bin_in[7:4];  end
   endcase
   
   an[3:2] = 2'b11; //disable top 2 anode digits
 end // End Always block 1
 
-always @(digit_to_Display)begin
-  case (digit_to_Display) // Activate Cathodes per digit. ACTIVE LOW!!
-  4'b0000:  segs = 7'b1000000; // 0
-  4'b0001:  segs = 7'b1111001; // 1
-  4'b0010:  segs = 7'b0100100; // 2
-  4'b0011:  segs = 7'b0110000; // 3
-  4'b0100:  segs = 7'b0011001; // 4
-  4'b0101:  segs = 7'b0010010; // 5
-  4'b0110:  segs = 7'b0000010; // 6
-  4'b0111:  segs = 7'b1111000; // 7
-  4'b1000:  segs = 7'b0000000; // 8
-  4'b1001:  segs = 7'b0011000; // 9
-  4'hA:     segs = 7'b0001000; // A
-  4'hB:     segs = 7'b0000011; // b
-  4'hC:     segs = 7'b1000110; // C
-  4'hD:     segs = 7'b0100001; // d
-  4'hE:     segs = 7'b0000110; // E
-  4'hF:     segs = 7'b0001110; // F
+always @(digit_to_display) begin
+  case (digit_to_display) // Activate Cathodes per digit. ACTIVE LOW!!
+  4'h0: segs = 7'b1000000; // 0
+  4'h1: segs = 7'b1111001; // 1
+  4'h2: segs = 7'b0100100; // 2
+  4'h3: segs = 7'b0110000; // 3
+  4'h4: segs = 7'b0011001; // 4
+  4'h5: segs = 7'b0010010; // 5
+  4'h6: segs = 7'b0000010; // 6
+  4'h7: segs = 7'b1111000; // 7
+  4'h8: segs = 7'b0000000; // 8
+  4'h9: segs = 7'b0011000; // 9
+  4'hA: segs = 7'b0001000; // A
+  4'hB: segs = 7'b0000011; // b
+  4'hC: segs = 7'b1000110; // C
+  4'hD: segs = 7'b0100001; // d
+  4'hE: segs = 7'b0000110; // E
+  4'hF: segs = 7'b0001110; // F
   default:  segs = 7'b1111111;           
   endcase
 end
--- lab6/controller.v
+++ lab6/controller.v
@@ -1,22 +1,70 @@
 module controller(clk, cs, we, address, data_in, data_out, mode, btns_db, swtchs, leds, segs, an);
-  input clk;
-  output cs;
-  output we;
-  output[6:0] address;
-  input [7:0] data_in;
-  output[7:0] data_out; 
-  input [1:0] mode;
-  input [1:0] btns_db;
-  input [7:0] swtchs;
-  output[7:0] leds;
-  output[6:0] segs;
-  output[3:0] an;
+input clk;
 
+input [7:0] data_in; //in from memory aka DVR
+input [1:0] mode; //SW15,SW14
+input [1:0] btns_db;//btnL,btnR
+input [7:0] swtchs;//switch numerical input
 
-  //WRITE THE FUNCTION OF THE CONTROLLER
+output cs; //channel select
+output we; //write enable
+output[6:0] address; //mem addr aka DAR
+output reg [7:0] data_out; //out to data bus 
+output[7:0] leds; //address display
+output[6:0] segs; //7seg value cathodes
+output[3:0] an; //7seg value anodes
 
-always @(posedge clk) begin
+wire clk_500Hz; //for 7seg
 
+reg [6:0] SPR,DAR;
+reg [7:0] DVR;
+
+initial begin 
+  SPR <= 7'h7F;
+  DAR <= 7'h00;
 end
+
+assign cs = 1'b1; //never changes
+assign address = DAR;
+
+//WRITE THE FUNCTION OF THE CONTROLLER
+always @(posedge clk) begin
+  we <= 1'b0;
+  if (we == 1'b0) begin DVR <= data_in; end
+
+  case({mode,btns_db})
+  4'b0010: begin //pop 
+    SPR <= SPR +1;
+    DAR <= SPR +1;
+  end
+  4'b0001: begin //push
+    we <= 1'b1;
+    data_out <= swtchs;
+    SPR <= SPR -1;
+    DAR <= SPR +1;
+  end
+  4'b0110: begin //sub
+  end
+  4'b0101: begin //add
+  end
+  4'b1010: begin //clr/RST
+    SPR <= 7'h7F;
+    DAR <= 7'h00;
+    data_out <= 8'h00;
+    we <= 1'b1;
+  end
+  4'b1001: begin //top
+    DAR <= SPR + 1;
+  end
+  4'b1110: begin //decrement
+  end
+  4'b1101: begin //increment
+  end
+  default: ; //do nothing
+  endcase
+end
+
+Four_Digit_7_seg_Display two_digit (bin_in,clk_500Hz,an,segs);
+Divider div100Hz (clk,28'd100000,clk_500Hz);
 endmodule
 
